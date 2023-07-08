3493922af05288ad8614e8c497522b75f04adefb
Johnny Proano <jproano22@yahoo.com>
2023-07-07 23:27:27 -0400

    added header folder, nav bar, and links

---
+++ src/header/header.jsx
@@ -0,0 +1,15 @@
+export default function Header() {
+  return (
+    <div>
+      <div className="header-container">
+        {/* <img src="src/assets/image-logo-black.png" /> */}
+        <nav>
+          <a href="#">Home</a>
+          <a href="#">Bio</a>
+          <a href="#">Music</a>
+          <a href="#">Contact</a>
+        </nav>
+      </div>
+    </div>
+  );
+}
