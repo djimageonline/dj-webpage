d8ab47f3f46466e3004df73f3c2b3a8549a87c0c
Johnny Proano <jproano22@yahoo.com>
2023-07-10 23:01:02 -0400

    added backgorund image url and mixcloud.  Adjusted navbar and icons for social media

--- src/home/home.jsx
+++ src/home/home.jsx
@@ -1,19 +1,40 @@
 import "./home.css";
 
+import { FaMixcloud, FaTiktok, FaInstagramSquare } from "react-icons/fa";
+
 export default function Home() {
   return (
     <div className="home-container">
-      <div className="image-hero-container">
-        <div className="image-shape-container">
-          <img className="image-main" src="src/assets/image-main.png" />
+      <div className="image-shape-container">
+        <div className="text">
+          <h1>DEEJAY IMAGE</h1>
+          <p>Hi, I'm a DJ formerly from Chicago, IL now residing in Orlando, FL</p>
         </div>
       </div>
       <div className="text-container">
-        <h1>DEEJAY IMAGE</h1>
-        <div>
-          <a></a>
+        <div className="icon-list">
+          <a>
+            <FaMixcloud className="icon" />
+          </a>
+          <a>
+            <FaInstagramSquare className="icon" />
+          </a>
+          <a>
+            <FaTiktok className="icon" />
+          </a>
+          <a>
+            <FaMixcloud className="icon" />
+          </a>
         </div>
       </div>
+
+      <iframe
+        width="100%"
+        height="400"
+        src="https://www.mixcloud.com/widget/iframe/?light=1&autoplay=1&feed=%2Fdjimageonline%2F"
+        frameborder="0"
+        allow="autoplay"
+      ></iframe>
     </div>
   );
 }
